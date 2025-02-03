f6614fa6a08a54fdbb4327bcc171b9c0d59c465c
Tomas Afonso <tafonso@c1r2s3.42lisboa.com>
2025-02-03 18:30:35 +0000

    The shells and C00 that was giving an error

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tafonso <tafonso@student.42lisboa.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/22 15:12:04 by tafonso           #+#    #+#             */
+/*   Updated: 2025/01/23 10:25:21 by tafonso          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	write (1, "abcdefghijklmnopqrstuvwxyz", 26);
+}
+
+/*void	main(void)
+{
+	ft_print_alphabet();
+}
+*/
---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tafonso <tafonso@student.42lisboa.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/22 17:48:28 by tafonso           #+#    #+#             */
+/*   Updated: 2025/01/23 15:56:58 by tafonso          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	write (1, "zyxwvutsrqponmlkjihgfedcba", 26);
+}
+
+/*void	main(void)
+{
+	ft_print_reverse_alphabet();
+}
+*/
